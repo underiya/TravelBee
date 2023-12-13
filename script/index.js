f6de7920568a48ff1c41d7ed4e7bc109b02b45b1
Deepak Underiya <underiyadeepak@gmail.com>
2023-12-13 15:05:20 +0530

    day2

---
+++ script/index.js
@@ -0,0 +1,7 @@
+import { nav } from "./navbar.js";
+
+let navbar=document.querySelector(".navbar")
+navbar.innerHTML=nav;
+
+
+
---
+++ script/navbar.js
@@ -0,0 +1,20 @@
+
+ let nav=` 
+      <a href="#" class="logo">
+      <img src="./images/Travel Bee.png" alt="logo">
+      </a>
+      <ul class="nav-list">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+      <div class="nav-actions">
+        <a href="#" class="button loginBtn">Login</a>
+        <a href="#" class="button registerBtn">Register</a>
+      </div>
+    `
+
+    export {nav};
+    
+
