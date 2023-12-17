7e67ccd66faa86aecf9745883f7f5ad7e7029b7a
Deepak Underiya <underiyadeepak@gmail.com>
2023-12-18 03:25:59 +0530

    day5

--- script/navbar.js
+++ script/navbar.js
@@ -1,17 +1,17 @@
 
  let nav=` 
-      <a href="./index.html" class="logo">
-      <img src="./images/Travel Bee.png" alt="logo">
+      <a href="index.html" id="logo">
+      <img src="pics/clear logo-crop.png" alt="Logo" class="logo"/>
       </a>
-      <ul class="nav-list">
-        <li><a href="">Home</a></li>
-        <li><a href="./about.html">About</a></li>
-        <li><a href="./services.html">Services</a></li>
-        <li><a href="#">Your Package</a></li>
-      </ul>
-      <div class="nav-actions">
-        <a href="./login.html" class="button loginBtn">Login</a>
-        <a href="./register.html" class="button registerBtn">Register</a>
+      <div class="nav-links">
+      <a href="./index.html">Home</a>
+      <a href="./about.html">About</a>
+      <a href="./services.html">Services</a>
+      <a href="#">Your Package</a>
+      </div>
+      <div class="user-actions">
+      <a href="login.html">Login</a>
+      <a href="register.html">Register</a>
       </div>
     `
 
