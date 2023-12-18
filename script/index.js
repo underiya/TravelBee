7edfc7101adee995e215e844f68441fa0384cba0
Deepak Underiya <underiyadeepak@gmail.com>
2023-12-18 16:49:29 +0530

    correction

--- script/index.js
+++ script/index.js
@@ -8,3 +8,22 @@ navbar.innerHTML=nav;
 let foot=document.querySelector("footer");
 foot.innerHTML=footer;
 
+
+
+function displayPopup() {
+    setTimeout(function () {
+        document.getElementById('popup-container').style.display = 'block';
+      }, 3000);
+    }
+  
+    // Function to close the popup
+    function closePopup() {
+      document.getElementById('popup-container').style.display = 'none';
+      document.body.style.overflow = 'auto'; // Allow scrolling when the popup is closed
+    }
+  
+    // Call the displayPopup function when the page loads
+    window.onload = displayPopup;
+  
+    // Attach the closePopup function to the close button
+    document.getElementById('close-btn').addEventListener('click', closePopup);
